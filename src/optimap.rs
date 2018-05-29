de8d13624e8c4bb8808db5799d5e84c5a785ad6c
Ogier Bouvier <ars3nic@ars3nic.ch>
2018-05-29 16:24:22 +0200

    update benchmark suite and use new version of abox

--- src/optimap.rs
+++ src/optimap.rs
@@ -95,13 +95,13 @@ impl<K: PartialEq, V: PartialEq> PartialEq for ValueHolder<K, V> {
 }
 
 struct AtomicVersionTable<K, V> {
-    value: AtomicBox<Vec<Arc<BucketEntry<K, V>>>>,
+    value: Arc<AtomicBox<Vec<Arc<BucketEntry<K, V>>>>>,
 }
 
 impl<K: PartialEq, V> AtomicVersionTable<K, V> {
     fn new() -> AtomicVersionTable<K, V> {
         AtomicVersionTable {
-            value: AtomicBox::new(Vec::new()),
+            value: Arc::new(AtomicBox::new(Vec::new())),
         }
     }
 
@@ -110,7 +110,7 @@ impl<K: PartialEq, V> AtomicVersionTable<K, V> {
         let bucket = Arc::new(BucketEntry::new(key, value));
 
         self.value.replace_with(move |x| {
-            let mut y = x.clone();
+            let mut y = (*x).clone();
 
             for i in 0..y.len() {
                 if y[i].key_matches(bucket.key()) {
@@ -127,13 +127,11 @@ impl<K: PartialEq, V> AtomicVersionTable<K, V> {
     #[inline]
     fn delete(&self, key: &K) {
         self.value.replace_with(move |x| {
-            let mut y = x.clone();
+            let mut y = Vec::new();
 
-            for i in 0..y.len() {
-                if y[i].key_matches(key) {
-                    // FIXME: should not shift all elements in the vector
-                    y.remove(i);
-                    break;
+            for i in 0..x.len() {
+                if !x[i].key_matches(key) {
+                    y.push(Arc::clone(&x[i]));
                 }
             }
 
@@ -148,9 +146,9 @@ impl<K: PartialEq, V> AtomicVersionTable<K, V> {
 
     #[inline]
     fn find<F>(&self, f: F) -> Option<Arc<BucketEntry<K, V>>>
-        where F: Fn(&Arc<BucketEntry<K, V>>) -> bool
+        where F: FnMut(&Arc<BucketEntry<K, V>>) -> bool,
     {
-        self.value.iter().cloned().find(f)
+        self.value.get().iter().cloned().find(f)
     }
 }
 
@@ -279,16 +277,16 @@ pub mod tests {
     use std::collections::hash_map::RandomState;
     use std::fmt;
     use std::fs::File;
-    use std::hash::{BuildHasher, Hash};
+    use std::hash::Hash;
     use std::io::Write;
     use std::marker::PhantomData;
     use std::time::SystemTime;
-    use std::sync::{Arc, RwLock};
-    use std::sync::atomic::{AtomicUsize, Ordering};
-    use std::thread;
+    use std::sync::{Arc, Barrier, RwLock};
 
+    use self::crossbeam_utils::scoped::ScopedJoinHandle;
     use self::crossbeam_utils::scoped;
     use self::rand::Rng;
+    use self::rand::distributions::{Distribution, Standard};
     use self::test::Bencher;
 
     use logmap::tests::gen_rand_strings;
@@ -318,7 +316,7 @@ pub mod tests {
 
         let out = table.get(&key).expect("value was not inserted");
 
-        assert_eq!(out.key_matches(&key), true);
+        assert!(out.key_matches(&key));
         assert_eq!(out.value_ref(), &value);
     }
 
@@ -510,16 +508,18 @@ pub mod tests {
             scoped::scope(|ctx| {
                 let results = Arc::clone(&results);
                 let rd = Arc::clone(&results);
+                let barrier = Arc::new(Barrier::new(core_ids.len() - 2));
 
-                core_ids.into_iter().zip(workload.iter().cloned())
+                let guards: Vec<ScopedJoinHandle<_>> = core_ids.into_iter().zip(workload.iter().cloned())
                     .skip(1)
                     .map(|(id, work)| {
                         core_affinity::set_for_current(id);
                         let results = Arc::clone(&results);
                         let map = Arc::clone(&map);
-                        let work = work.clone();
+                        let b = Arc::clone(&barrier);
 
                         ctx.spawn(move || {
+                            b.wait();
                             let times = work
                                 .iter()
                                 .cloned()
@@ -527,7 +527,11 @@ pub mod tests {
                                 .collect();
                             results.write().unwrap().push(times);
                         })
-                    }).for_each(drop);
+                    }).collect();
+
+                for g in guards {
+                    g.join().unwrap();
+                }
 
                 let mut file = File::create(name).unwrap();
 
@@ -541,26 +545,92 @@ pub mod tests {
         }
     }
 
+    fn gen_workload<K: PartialEq + Clone, V>(keys: Vec<K>, put: usize,
+                                             get: usize, del: usize) -> Vec<Arc<Vec<Task<K, V>>>>
+        where
+        Standard: Distribution<V>,
+    {
+        let mut rng = rand::thread_rng();
+        let mut workloads = core_affinity::get_core_ids().unwrap().into_iter()
+            .skip(1) // leave one core for Linux
+            .map(|_| {
+                let tasks = (0..(put + get + del)).map(|i| {
+                    let key = keys[rng.gen::<usize>() % keys.len()].clone();
+
+                    if i < put {
+                        Task::Get(key)
+                    } else if i < put + get {
+                        Task::Put(key, rng.gen::<V>())
+                    } else {
+                        Task::Remove(key)
+                    }
+                }).collect::<Vec<_>>();
+
+                Arc::new(tasks)
+            }).collect::<Vec<Arc<_>>>();
+
+        for mut workload in &mut workloads {
+            let mut slice = Arc::get_mut(workload).unwrap().as_mut_slice();
+            rng.shuffle(slice);
+        }
+
+        workloads
+    }
+
+    #[test]
+    fn optimap_read_heavy_benchmark() {
+        let key_count = 8192;
+        let esize = 1024;
+        let keys = gen_rand_strings(key_count, esize);
+        let put_count = 500000;
+        let get_count = 1000000;
+        let del_count = 5000;
+
+        let mut rng = rand::thread_rng();
+        let init = keys.iter().cloned().map(|k| Task::Put(k, rng.gen::<u64>())).collect();
+        let workloads = gen_workload::<String, u64>(keys, put_count, get_count, del_count);
+
+        BenchRunner::run_benchmark("read_heavy.csv", key_count, workloads, init);
+    }
+
+    #[test]
+    fn optimap_no_collision_benchmark() {
+        let key_size = 128;
+        let key_count = 8192;
+        let keys = gen_rand_strings(key_count, key_size);
+
+        let put_count = 1000000;
+        let get_count = 1000000;
+        let del_count = 5000;
+
+        let mut rng = rand::thread_rng();
+        let init = keys.iter().cloned().map(|k| Task::Put(k, rng.gen::<u64>())).collect();
+        let workloads = gen_workload::<String, u64>(keys, put_count, get_count, del_count);
+
+        BenchRunner::run_benchmark("balanced_no_collision", key_count * 2, workloads, init);
+    }
+
     #[test]
     // TODO: improve this by balancing puts and gets across cores
-    fn optimap_2x_collision_benchmark() {
+    fn optimap_collision_benchmark() {
         let mut rng = rand::thread_rng();
 
         // test parameters
-        let value_count = 8192;
+        let key_count = 8192;
         let key_size = 128;
-        let bucket_count = value_count / 2;
-        let put_count: usize = 500000;
-        let get_count: usize = 500000;
+        let bucket_count = key_count / 4;
+        let put_count: usize = 1000000;
+        let get_count: usize = 1000000;
         let del_count: usize = 50000;
 
-        let keys = gen_rand_strings(value_count, key_size);
+        let keys = gen_rand_strings(key_count, key_size);
 
         let init = keys.iter()
             .map(|k| Task::Put(k.clone(), rng.gen::<u64>()))
             .collect::<Vec<_>>();
 
         let workloads = core_affinity::get_core_ids().unwrap().into_iter()
+            .skip(1)
             .map(|_| {
                 let tasks = (0..(put_count + get_count + del_count)).map(|i| {
                     let key = keys[rng.gen::<usize>() % keys.len()].clone();
@@ -577,6 +647,6 @@ pub mod tests {
                 Arc::new(tasks)
             }).collect::<Vec<Arc<_>>>();
 
-        BenchRunner::run_benchmark("128_5MG.5MP.5KD", bucket_count, workloads, init);
+        BenchRunner::run_benchmark("balanced_collision", bucket_count, workloads, init);
     }
 }
